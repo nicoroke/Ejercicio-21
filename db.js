ed6a01fc41f25f5e0d5ffed1f078af44ef31cbbf
dsfpablo <dsfpablo@gmail.com>
2023-02-09 15:15:16 -0300

    update db structure

--- db.js
+++ db.js
@@ -9,5 +9,82 @@ const sequelize = new Sequelize(
     dialect: process.env.DB_CONNECTION,
   }
 );
-sequelize.authenticate();
-console.log("ok");
+
+class Article extends Model {}
+Article.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    author: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "article" }
+);
+
+class Comment extends Model {}
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    article_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "comment" }
+);
+
+class User extends Model {}
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    firstname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "comment" }
+);

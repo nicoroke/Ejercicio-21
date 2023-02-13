065c9f557deda5c7c4ea33f0c931a6dbfa0bdd9e
dsfpablo <dsfpablo@gmail.com>
2023-02-13 16:29:00 -0300

    que se lo que dios quiera

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,5 +4,8 @@ const apiController = require("../controllers/apiController");
 
 router.get("/articulos/", apiController.showAll);
 router.get("/articulos/:id", apiController.showOneById);
+// router.post("/articulo", apiController.store);
+// router.patch("/articulo/:id", apiController.update);
+// router.delete("/articulo/:id", apiController.destroy);
 
 module.exports = router;
